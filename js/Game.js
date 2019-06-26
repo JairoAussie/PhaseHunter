6b7e3eb3dd495d557cefe58d20d21a8584626290
Jairo Bilbao <jairoaussie@gmail.com>
2019-06-26 17:10:30 +0200

    Creating classes and constructors

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor (){
+        this.missed=0;
+        this.phrases= [
+            new Phrase('Fix the cause not the symptom'),
+            new Phrase('Knowledge is power'),
+            new Phrase('Simplicity is the soul of efficiency'),
+            new Phrase('Before software can be reusable it first has to be usable'),
+            new Phrase('Experience is the name everyone gives to their mistakes')
+        ];
+        this.activePhrase=null;
+
+    }
+    
+};
--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+const phrase = new Phrase('Try to guess the phrase');
+console.log(phrase.phrase);
+const game = new Game();
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
